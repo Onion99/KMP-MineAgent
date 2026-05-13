f82d92539d75f05371082491c4dcb145585b6b9f
OnionTiger <lijundong9999@gmail.com>
2026-05-13 11:29:03 +0800

    feat: Fixed message update issue

--- data-model/src/commonMain/kotlin/com/onion/model/ChatMessage.kt
+++ data-model/src/commonMain/kotlin/com/onion/model/ChatMessage.kt
@@ -9,19 +9,4 @@ data class ChatMessage(
         val videoFrames: List<ByteArray>? = null,
         val metadata: Map<String, String>? = null,
         val id: Long = Random.nextLong()
-) {
-        override fun equals(other: Any?): Boolean {
-                if (this === other) return true
-                if (other == null || this::class != other::class) return false
-
-                other as ChatMessage
-
-                if (id != other.id) return false
-
-                return true
-        }
-
-        override fun hashCode(): Int {
-                return id.hashCode()
-        }
-}
+)
